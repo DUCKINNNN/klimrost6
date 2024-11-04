5e2e759672e915c310c4b96fcd3605d392f7a174
klimr <klimrost6@gmail.com>
2024-11-04 19:31:38 +0200

    Add app description

--- src/klimrost6/MyGitApp.java
+++ src/klimrost6/MyGitApp.java
@@ -3,6 +3,7 @@ package klimrost6;
 public class MyGitApp {
     public static void main(String[] args) {
         System.out.println("My git app for Users. ");
+        System.out.println("The informative application.");
     }
 
 }
