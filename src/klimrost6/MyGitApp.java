62ff9c0c7c5be9b7103fe0b33de148649b203630
klimr <klimrost6@gmail.com>
2024-11-04 19:23:26 +0200

    Initial commit

---
+++ src/klimrost6/MyGitApp.java
@@ -0,0 +1,8 @@
+package klimrost6;
+
+public class MyGitApp {
+    public static void main(String[] args) {
+        System.out.println("My git app. ");
+    }
+
+}
---
+++ src/klimrost6/main.java
@@ -0,0 +1,51 @@
+package klimrost6;
+
+public class main {
+    public static void main(String[] args) {
+        String clientOne = " Alice.";
+        String product = "smartphone;";
+        double price = 305.99;
+        String address = "Moon Street, 10.";
+
+        System.out.println("Order No 1 Client:" + clientOne);
+        System.out.println("Product:" + product);
+        System.out.println("Price:" + price);
+        System.out.println("Address:" + address);
+
+        String clientTwo = "Tom.";
+        String productTwo = "laptop;";
+        double priceTwo = 570.95;
+        String addressTwo = "Terra Street, 17.";
+
+        System.out.println("Order No 2 Client:" + clientTwo);
+        System.out.println("Product:" + productTwo);
+        System.out.println("Price:" + priceTwo);
+        System.out.println("addressTwo = " + addressTwo);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Order No 1 Client: Alice.
+Product: smartphone,
+price EUR 305.99.
+Address: Moon Street, 10.
+
+Order No 2 Client: Tom.
+Product: laptop,
+price EUR 570.95.
+Address: Terra Street, 17.
+*/
