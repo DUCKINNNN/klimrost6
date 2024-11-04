f0ebe6a9ee72f7bcc076c0e5a7555910c1748124
klimr <klimrost6@gmail.com>
2024-11-04 20:02:25 +0200

    Add update app shop

--- src/klimrost6/MyGitApp.java
+++ src/klimrost6/MyGitApp.java
@@ -2,7 +2,7 @@ package klimrost6;
 
 public class MyGitApp {
     public static void main(String[] args) {
-        System.out.println("This is shop-online app.");
+        System.out.println("Shop-online for customers.");
 
     }
 
